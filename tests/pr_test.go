fa26ab20cb59da75809f906846b174db592e96cb
Vincent Burckhardt <vburckhardt@gmail.com>
2024-07-04 14:40:30 +0100

    Revert "test: update ibmcloud-terratest-wrapper to v1.35.3 (#144)"
    This reverts commit c31aa94360db50ba139747844e220a98dda1f0ef.

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -9,9 +9,18 @@ import (
 
 func TestProjectsFullTest(t *testing.T) {
 	options := testprojects.TestProjectOptionsDefault(&testprojects.TestProjectsOptions{
-		Testing:        t,
-		Prefix:         "rag-stack",
-		ParallelDeploy: true,
+		Testing: t,
+		Prefix:  "rag-stack",
+		StackConfigurationOrder: []string{
+			"1 - Account Infrastructure Base",
+			"2a - Security Service - Key Management",
+			"2b - Security Service - Secret Manager",
+			"2c - Security Service - Security Compliance Center",
+			"3 - Observability - Logging Monitoring Activity Tracker",
+			"4 - WatsonX SaaS services",
+			"5 - Sample RAG app - Application Lifecycle Management",
+			"6 - Sample RAG app configuration",
+		},
 	})
 
 	privateKey, _, kerr := common.GenerateTempGPGKeyPairBase64()

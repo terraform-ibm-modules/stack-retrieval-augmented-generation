33748fde76b0987b2c45898b3ab8d035d5d25f0c
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-09-08 08:06:52 +0100

    chore(deps): update terraform-module (#185)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -16,7 +16,7 @@ module "resource_group" {
 
 module "event_notifications" {
   source            = "terraform-ibm-modules/event-notifications/ibm"
-  version           = "1.10.6"
+  version           = "1.10.10"
   resource_group_id = module.resource_group.resource_group_id
   name              = "${var.prefix}-en"
   tags              = var.resource_tags
@@ -31,7 +31,7 @@ module "event_notifications" {
 
 module "secrets_manager" {
   source               = "terraform-ibm-modules/secrets-manager/ibm"
-  version              = "1.17.10"
+  version              = "1.17.12"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   secrets_manager_name = "${var.prefix}-secrets-manager" #tfsec:ignore:general-secrets-no-plaintext-exposure
@@ -45,7 +45,7 @@ module "secrets_manager" {
 
 module "key_protect_all_inclusive" {
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "4.15.9"
+  version                     = "4.15.11"
   resource_group_id           = module.resource_group.resource_group_id
   key_protect_instance_name   = "${var.prefix}-kms"
   region                      = var.region

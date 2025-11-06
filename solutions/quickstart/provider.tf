b7fc64b197c1775c6e6a07ea335d4b7ce387c2aa
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-11-07 05:17:52 +0530

    removed es-index provider config

--- solutions/quickstart/provider.tf
+++ solutions/quickstart/provider.tf
@@ -5,18 +5,6 @@ provider "ibm" {
   private_endpoint_type = (var.provider_visibility == "private" && var.region == "ca-mon") ? "vpe" : null
 }
 
-provider "elasticsearch" {
-  alias                 = "ibm_es"
-  username              = module.icd_elasticsearch.service_credentials_object.credentials["elasticsearch_admin"].username
-  password              = module.icd_elasticsearch.service_credentials_object.credentials["elasticsearch_admin"].password
-  url                   = "https://${module.icd_elasticsearch.service_credentials_object.hostname}:${module.icd_elasticsearch.service_credentials_object.port}"
-  cacert_file           = base64decode(module.icd_elasticsearch.service_credentials_object.certificate)
-  elasticsearch_version = "8.12"
-
-  # Below is required to avoid HEAD healthcheck failed error. Reference to similar issue and suggestion - https://github.com/phillbaker/terraform-provider-elasticsearch/issues/352
-  healthcheck = false
-}
-
 provider "restapi" {
   uri                  = "https:"
   write_returns_object = true

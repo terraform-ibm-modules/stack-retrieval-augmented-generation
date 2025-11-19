0821cfc9b702a743a2e8248d03908138dcc41326
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2025-11-19 12:11:00 +0530

    fix: updated qs changes

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -8,8 +8,9 @@ locals {
   service_endpoints = "public-and-private"
 
   # KMS
-  key_ring_name = "${local.prefix}keyring"
-  key_name      = "${local.prefix}key"
+  key_ring_name    = "${local.prefix}keyring"
+  key_name         = "${local.prefix}key"
+  wx_data_key_name = "${local.prefix}wxd-key"
 
   # ICD Elastic Search
   es_credentials = {
@@ -21,10 +22,13 @@ locals {
 
   # Watson
   watson_plan = {
-    "studio"    = "professional-v1",
-    "runtime"   = "v2-professional",
-    "discovery" = "plus",
-    "assistant" = "enterprise"
+    "studio"      = "professional-v1",
+    "runtime"     = "v2-professional",
+    "discovery"   = "plus",
+    "assistant"   = "enterprise",
+    "governance"  = "essentials",
+    "data"        = "lakehouse-enterprise",
+    "orchestrate" = "essentials-agentic-mau"
   }
 
 }
@@ -58,7 +62,7 @@ module "cos" {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "5.4.5"
+  version                   = "5.5.0"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${local.prefix}kp"
@@ -69,6 +73,10 @@ module "key_protect_all_inclusive" {
       keys = [
         {
           key_name     = local.key_name
+          force_delete = true
+        },
+        {
+          key_name     = local.wx_data_key_name
           force_delete = true
         }
       ]
@@ -83,22 +91,22 @@ module "key_protect_all_inclusive" {
 data "ibm_iam_auth_token" "restapi" {}
 
 module "watsonx_ai" {
-  source                           = "terraform-ibm-modules/watsonx-ai/ibm"
-  version                          = "2.9.1"
-  region                           = var.region
-  resource_group_id                = module.resource_group.resource_group_id
-  resource_tags                    = var.resource_tags
-  project_name                     = "${local.prefix}project-rag"
+  source            = "terraform-ibm-modules/watsonx-ai/ibm"
+  version           = "2.10.1"
+  region            = var.region
+  resource_group_id = module.resource_group.resource_group_id
+  resource_tags     = var.resource_tags
+  project_name      = "${local.prefix}project-cnai"
 
-  watsonx_ai_studio_instance_name  = "${local.prefix}wx-studio"
-  watsonx_ai_studio_plan           = local.watson_plan["studio"]
+  watsonx_ai_studio_instance_name = "${local.prefix}wx-studio"
+  watsonx_ai_studio_plan          = local.watson_plan["studio"]
 
   watsonx_ai_runtime_instance_name = "${local.prefix}wx-runtime"
   watsonx_ai_runtime_plan          = local.watson_plan["runtime"]
 
-  enable_cos_kms_encryption        = true
-  cos_instance_crn                 = module.cos.cos_instance_crn
-  cos_kms_key_crn                  = module.key_protect_all_inclusive.keys["${local.key_ring_name}.${local.key_name}"].crn
+  enable_cos_kms_encryption = true
+  cos_instance_crn          = module.cos.cos_instance_crn
+  cos_kms_key_crn           = module.key_protect_all_inclusive.keys["${local.key_ring_name}.${local.key_name}"].crn
 }
 
 ##############################################################################################################
@@ -132,6 +140,52 @@ module "watsonx_assistant" {
 }
 
 ##############################################################################################################
+# watsonx.governance
+##############################################################################################################
+
+module "watsonx_governance" {
+  source                  = "terraform-ibm-modules/watsonx-governance/ibm"
+  version                 = "1.11.6"
+  region                  = var.region
+  resource_group_id       = module.resource_group.resource_group_id
+  plan                    = local.watson_plan["governance"]
+  watsonx_governance_name = "${local.prefix}governance"
+  resource_tags           = var.resource_tags
+}
+
+##############################################################################################################
+# watsonx.data
+##############################################################################################################
+
+module "watsonx_data" {
+  source                        = "terraform-ibm-modules/watsonx-data/ibm"
+  version                       = "1.12.7"
+  region                        = var.region
+  resource_group_id             = module.resource_group.resource_group_id
+  watsonx_data_name             = "${local.prefix}data"
+  plan                          = local.watson_plan["data"]
+  use_case                      = "workloads"
+  resource_tags                 = var.resource_tags
+  enable_kms_encryption         = true
+  skip_iam_authorization_policy = false
+  watsonx_data_kms_key_crn      = module.key_protect_all_inclusive.keys["${local.key_ring_name}.${local.wx_data_key_name}"].crn
+}
+
+##############################################################################################################
+# watsonx Orchestrate
+##############################################################################################################
+
+module "watsonx_orchestrate" {
+  source                   = "terraform-ibm-modules/watsonx-orchestrate/ibm"
+  version                  = "1.11.6"
+  region                   = var.region
+  resource_group_id        = module.resource_group.resource_group_id
+  watsonx_orchestrate_name = "${local.prefix}orchestrate"
+  plan                     = local.watson_plan["orchestrate"]
+  resource_tags            = var.resource_tags
+}
+
+##############################################################################################################
 # Elastic search
 ##############################################################################################################
 
@@ -142,7 +196,7 @@ module "icd_elasticsearch" {
   name                     = "${local.prefix}data-store"
   region                   = var.region
   plan                     = "enterprise"
-  elasticsearch_version    = "8.12"
+  elasticsearch_version    = "8.15"
   tags                     = var.resource_tags
   service_endpoints        = "private"
   member_host_flavor       = "multitenant"
@@ -161,16 +215,27 @@ module "icr_namespace" {
   namespace_name    = "${local.prefix}namespace"
 }
 
+module "cr_endpoint" {
+  source  = "terraform-ibm-modules/container-registry/ibm//modules/endpoint"
+  version = "2.3.5"
+  region  = var.region
+}
+
 ##############################################################################################################
 # Code Engine - Project, Secret, Build, Application
 ##############################################################################################################
 
 locals {
-  env_vars = [
+  env_vars = [{
+    type      = "secret_key_reference"
+    name      = "WATSONX_AI_APIKEY"
+    key       = "WATSONX_AI_APIKEY"
+    reference = "wx-ai"
+    },
     {
       type  = "literal"
-      name  = "WATSONX_AI_APIKEY"
-      value = var.ibmcloud_api_key
+      name  = "WATSONX_SERVICE_URL"
+      value = "https://${var.region}.ml.cloud.ibm.com"
     },
     {
       type  = "literal"
@@ -193,36 +258,41 @@ locals {
       value = "true"
     }
   ]
-  cr_region    = split("-", var.region)[0]
-  output_image = "private.${local.cr_region}.icr.io/${module.icr_namespace.namespace_name}/ai-agent-for-loan-risk"
+
+  ce_secrets = merge(
+    {
+      # Secret for Watsonx
+      "wx-ai" = {
+        format = "generic"
+        data = {
+          "WATSONX_AI_APIKEY" = var.ibmcloud_api_key
+        }
+      }
+    },
+    # Secret for Container Registry
+    {
+      "${local.prefix}registry-secret" = {
+        format = "registry"
+        data = {
+          username = "iamapikey"
+          password = var.ibmcloud_api_key
+          server   = module.cr_endpoint.container_registry_endpoint_private
+        }
+      }
+    }
+  )
+  source_url   = "https://github.com/IBM/ai-agent-for-loan-risk"
+  output_image = "${module.cr_endpoint.container_registry_endpoint_private}/${module.icr_namespace.namespace_name}/ai-agent-for-loan-risk"
   strategy     = "dockerfile"
   ce_app_name  = "ai-agent-for-loan-risk"
 }
 
-##############################################################################
-# Code Engine Project
-##############################################################################
-module "code_engine_project" {
-  source            = "terraform-ibm-modules/code-engine/ibm//modules/project"
-  version           = "4.6.4"
-  name              = "${local.prefix}project"
+module "code_engine" {
+  source            = "terraform-ibm-modules/code-engine/ibm"
+  version           = "4.6.12"
   resource_group_id = module.resource_group.resource_group_id
-}
-
-##############################################################################
-# Code Engine Secret
-##############################################################################
-module "code_engine_secret" {
-  source     = "terraform-ibm-modules/code-engine/ibm//modules/secret"
-  version    = "4.6.4"
-  name       = "${local.prefix}registry-secret"
-  project_id = module.code_engine_project.id
-  format     = "registry"
-  data = {
-    "server"   = "private.${local.cr_region}.icr.io",
-    "username" = "iamapikey",
-    "password" = var.ibmcloud_api_key,
-  }
+  project_name      = "${local.prefix}project"
+  secrets           = local.ce_secrets
 }
 
 ##############################################################################
@@ -231,36 +301,30 @@ module "code_engine_secret" {
 
 module "code_engine_build" {
   source                     = "terraform-ibm-modules/code-engine/ibm//modules/build"
-  version                    = "4.6.4"
+  version                    = "4.6.12"
   name                       = "${local.prefix}ce-build"
   ibmcloud_api_key           = var.ibmcloud_api_key
-  project_id                 = module.code_engine_project.id
+  project_id                 = module.code_engine.project_id
   existing_resource_group_id = module.resource_group.resource_group_id
   source_type                = "git"
-  source_url                 = "https://github.com/IBM/ai-agent-for-loan-risk"
-  strategy_type              = local.strategy
-  output_secret              = module.code_engine_secret.name
   output_image               = local.output_image
+  output_secret              = "${local.prefix}registry-secret"
+  source_url                 = local.source_url
   region                     = var.region
+  strategy_type              = local.strategy
 }
 
 ##############################################################################
 # Code Engine Application
 ##############################################################################
 module "code_engine_app" {
-  // Added dependency on Code Engine Build as first Image is required for the application.
-  depends_on                    = [module.code_engine_build]
-  source                        = "terraform-ibm-modules/code-engine/ibm//modules/app"
-  version                       = "4.6.4"
-  project_id                    = module.code_engine_project.id
-  name                          = local.ce_app_name
-  image_reference               = module.code_engine_build.output_image
-  image_secret                  = module.code_engine_secret.name
-  run_env_variables             = local.env_vars
-  scale_cpu_limit               = "4"
-  scale_memory_limit            = "32G"
-  scale_ephemeral_storage_limit = "300M"
-  managed_domain_mappings       = "local_private"
+  source            = "terraform-ibm-modules/code-engine/ibm//modules/app"
+  version           = "4.6.12"
+  project_id        = module.code_engine.project_id
+  name              = local.ce_app_name
+  image_reference   = module.code_engine_build.output_image
+  image_secret      = module.code_engine.secret["${local.prefix}registry-secret"].name
+  run_env_variables = local.env_vars
 }
 
-##############################################################################################################
+# #############################################################################################################
--- solutions/quickstart/outputs.tf
+++ solutions/quickstart/outputs.tf
@@ -83,6 +83,54 @@ output "watsonx_assistant" {
 }
 
 ########################################################################################################################
+# watsonx.governance
+########################################################################################################################
+
+output "watsonx_governance" {
+  description = "Connection and configuration details for the watsonx.governance instance."
+  value = {
+    "crn"           = module.watsonx_governance.crn
+    "account_id"    = module.watsonx_governance.account_id
+    "guid"          = module.watsonx_governance.guid
+    "name"          = module.watsonx_governance.name
+    "plan_id"       = module.watsonx_governance.plan_id
+    "dashboard_url" = module.watsonx_governance.dashboard_url
+  }
+}
+
+########################################################################################################################
+# watsonx.data
+########################################################################################################################
+
+output "watsonx_data" {
+  description = "Connection and configuration details for the watsonx.data instance."
+  value = {
+    "crn"           = module.watsonx_data.crn
+    "account_id"    = module.watsonx_data.account_id
+    "guid"          = module.watsonx_data.guid
+    "name"          = module.watsonx_data.name
+    "plan_id"       = module.watsonx_data.plan_id
+    "dashboard_url" = module.watsonx_data.dashboard_url
+  }
+}
+
+########################################################################################################################
+# watsonx.data
+########################################################################################################################
+
+output "watsonx_orchestrate" {
+  description = "Connection and configuration details for the watsonx Orchestrate instance."
+  value = {
+    "crn"           = module.watsonx_orchestrate.crn
+    "account_id"    = module.watsonx_orchestrate.account_id
+    "guid"          = module.watsonx_orchestrate.guid
+    "name"          = module.watsonx_orchestrate.name
+    "plan_id"       = module.watsonx_orchestrate.plan_id
+    "dashboard_url" = module.watsonx_orchestrate.dashboard_url
+  }
+}
+
+########################################################################################################################
 # ICD - ElasticSearch
 ########################################################################################################################
 
@@ -118,8 +166,10 @@ output "icr_namespace_crn" {
 output "code_engine" {
   description = "Code Engine output."
   value = {
-    "project_id"   = module.code_engine_project.id
+    "project_id"   = module.code_engine.project_id
     "app_url"      = module.code_engine_app.endpoint
     "output_image" = local.output_image
   }
 }
+
+########################################################################################################################
